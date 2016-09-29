46cc971b770a8ed990852cd0d9745f41a247a2a2
Adrian O'Grady <elpollouk@gmail.com>
2016-09-29 14:00:40 +0100

    Added a snazzy slideshow

---
+++ js/slideshow.js
@@ -0,0 +1,47 @@
+var imageDetails = [
+    { path:"slideshow/sun001.jpg", caption:"Sun spot activity in my back garden" },
+    { path:"slideshow/sun002.jpg", caption:"A closer look at the sun spots" },
+    { path:"slideshow/moon001.jpg", caption:"This is the moon as seen through my \xA35 webcam" },
+    { path:"slideshow/moon002.jpg", caption:"This is also the moon" },
+    { path:"slideshow/mars.png", caption:"One night trying to get a good picture of Mars" },
+    { path:"slideshow/jupiter.png", caption:"Various images of Jupiter I taken" },
+    { path:"slideshow/Docked.jpg", caption:"Cobra docked at an orbital outpost" },
+    { path:"slideshow/EarlyMorning.jpg", caption:"Early morning above Europe" },
+    { path:"slideshow/EuropaVista.jpg", caption:"Looking out over the canyons of Europa" },
+    { path:"slideshow/NightTimeLanding.jpg", caption:"A night time excursion" },
+    { path:"slideshow/Srv.jpg", caption:"Joy riding in my SRV" },
+    { path:"slideshow/SrvAndShip.jpg", caption:"Exploring a remote planetary landscape" }
+];
+
+var currentImage = 0;
+
+function setImage(index) {
+    viewerImg.src = imageDetails[index].path;
+    var caption = document.querySelector(".viewer .caption");
+    caption.innerText = imageDetails[index].caption;
+}
+
+function advanceSlide(offset) {
+    var nextImage = currentImage + offset;
+    if (nextImage < 0) {
+        nextImage = imageDetails.length - 1;
+    }
+    else if (nextImage >= imageDetails.length) {
+        nextImage = 0;
+    }
+
+    currentImage = nextImage;
+    setImage(currentImage);
+}
+
+function main() {
+    setImage(currentImage);
+}
+
+function previousButtonClicked() {
+    advanceSlide(-1);
+}
+
+function nextButtonClicked() {
+    advanceSlide(1);
+}
