b0e13f23bda23ac3c60e10a6710b1c09ae6117df
Adrian O'Grady <adriano@microsoft.com>
2018-02-13 11:48:40 -0800

    Updated to latest TwinStick

--- TwinStick/src/main.js
+++ TwinStick/src/main.js
@@ -2,6 +2,7 @@ Chicken.inject(["Config", "ConfigParser", "Game", "FieldVisualiser", "ChickenVis
 function (Config, parseConfig, Game, FieldVisualiser, UpdateLoop, Draw) {
     "use strict";
 
+    var draw;
     var visualiser;
     var game;
     var updater = new UpdateLoop((dt) => {
@@ -9,11 +10,35 @@ function (Config, parseConfig, Game, FieldVisualiser, UpdateLoop, Draw) {
         // visualiser.render();
     });
 
+    function resizeToFit() {
+        var heightRatio = window.innerHeight / draw.canvas.height
+        var widthRatio = window.innerWidth / draw.canvas.width
+
+        if (heightRatio < widthRatio) {
+            draw.canvas.style.width = '';
+            draw.canvas.style.height = '100%';
+            draw.canvas.style.top = '';
+            draw.canvas.style.marginTop = '';
+        }
+        else {
+            draw.canvas.style.width = '100%';
+            draw.canvas.style.height = '';
+            var halfHeight = draw.canvas.clientHeight / 2;
+            draw.canvas.style.top = '50%';
+            draw.canvas.style.marginTop = '-' + halfHeight + 'px';
+        }
+    }
+
     window.onload = () => {
         parseConfig();
-        var draw = new Draw(document.body, Config.game.width, Config.game.height);
+        draw = new Draw(document.body, Config.game.width, Config.game.height);
         game = new Game(draw);
         // visualiser = new FieldVisualiser(draw.context, document.body);
+
+        resizeToFit();
+
         updater.paused = false;
     }
+
+    window.onresize = resizeToFit;
 });
